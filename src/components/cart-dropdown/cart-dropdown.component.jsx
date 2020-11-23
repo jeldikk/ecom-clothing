ba1f5374314c37927f6bf305ae3d53d6c13ca0a2
jeldikk <jeldi.kamal2013@gmail.com>
2020-11-23 20:28:10 +0530

    cart icon addded, cartdropdown component and implemented reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./cart-dropdown.styles.scss"
+
+function CartDropdown() {
+    return (
+        <div className="cart-dropdown">
+            <div className="cart-items">
+
+            </div>
+            <button>Checkout</button>
+        </div>
+    )
+}
+
+export default CartDropdown
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,9 @@
+
+import CartActionTypes from "./cart.types"
+
+export const toggleDropDown = ()=>{
+
+    return {
+        type: CartActionTypes.TOGGLE_CART_DROPDOWN
+    }
+}
